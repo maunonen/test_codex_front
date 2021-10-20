ce5ef54cb6d98d225f8adf8580773ca20263b2d0
maunonen <santari33@gmail.com>
2021-10-20 13:33:15 +0300

    api base url changed to relative url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
-const instance = axios.create({
+/*const instance = axios.create({
     baseURL: 'http://localhost:3001/',
+})*/
+const instance = axios.create({
+    /*baseURL: 'http://localhost:3001/',*/
 })
 
 /**
